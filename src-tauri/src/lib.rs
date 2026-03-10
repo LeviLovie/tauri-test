1a0f1b2643b0078718f4e409de3d5c7cb5cb5dc4
Levi Lovie <leviiloviee@gmail.com>
2026-03-10 18:00:28 +0700

    Remove unused assignment

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,13 +8,12 @@ pub fn run() -> Result<()> {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
-            let scope = app.fs_scope();
             let base_path = app
                 .path()
                 .app_data_dir()
                 .context("failed to get app data directory")?;
             std::fs::create_dir_all(&base_path).context("failed to create base directory")?;
-            scope
+            app.fs_scope()
                 .allow_directory(base_path, false)
                 .context("failed to allow base directory")?;
             Ok(())
