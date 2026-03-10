0dad53d04b84b88b5508b12a2b3edecaca85aba7
Levi Lovie <leviiloviee@gmail.com>
2026-03-10 17:58:09 +0700

    Remove unused command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,12 @@
 use anyhow::{Context, Result};
-use tauri::{AppHandle, Manager};
+use tauri::Manager;
 use tauri_plugin_fs::FsExt;
-
-#[tauri::command]
-fn get_base_path(app: AppHandle) -> Result<String, String> {
-    let path = app.path().app_data_dir().map_err(|e| e.to_string())?;
-    Ok(path.to_string_lossy().to_string())
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> Result<()> {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_base_path])
         .setup(|app| {
             let scope = app.fs_scope();
             let base_path = app
